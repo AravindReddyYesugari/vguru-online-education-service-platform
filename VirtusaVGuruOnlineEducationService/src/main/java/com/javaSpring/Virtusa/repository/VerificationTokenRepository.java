9b8a1c6127ad2ef94fd56c62727a5c61dfd5ec25
YESUGARI ARAVIND <62026117+AravindReddyYesugari@users.noreply.github.com>
2020-09-02 14:19:28 +0530

    Update VerificationTokenRepository.java

--- VirtusaVGuruOnlineEducationService/src/main/java/com/javaSpring/Virtusa/repository/VerificationTokenRepository.java
+++ VirtusaVGuruOnlineEducationService/src/main/java/com/javaSpring/Virtusa/repository/VerificationTokenRepository.java
@@ -1,14 +1,1 @@
-package com.javaSpring.Virtusa.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.javaSpring.Virtusa.model.VerificationToken;
-
-
-@Repository
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-	Optional<VerificationToken> findByToken(String token);
-}
