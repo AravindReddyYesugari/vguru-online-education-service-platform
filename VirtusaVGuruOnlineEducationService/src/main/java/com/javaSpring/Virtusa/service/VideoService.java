02d10de3ae7bcd461021cd8365e82fe2c6578081
YESUGARI ARAVIND <62026117+AravindReddyYesugari@users.noreply.github.com>
2020-09-02 14:16:52 +0530

    Update VideoService.java

--- VirtusaVGuruOnlineEducationService/src/main/java/com/javaSpring/Virtusa/service/VideoService.java
+++ VirtusaVGuruOnlineEducationService/src/main/java/com/javaSpring/Virtusa/service/VideoService.java
@@ -1,19 +1,1 @@
-package com.javaSpring.Virtusa.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.javaSpring.Virtusa.model.VideoSection;
-import com.javaSpring.Virtusa.repository.VideoSectionRepository;
-
-import lombok.AllArgsConstructor;
-@Service
-@AllArgsConstructor
-@Transactional
-public class VideoService {
-private final VideoSectionRepository videoSR;
-	
-	public VideoSection getVideoLinks(String name) {
-		return videoSR.findByName(name);
-	}
-}
